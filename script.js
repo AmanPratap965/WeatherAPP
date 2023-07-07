07b8889db79dc85e8f32d64ab0ec1b5b60d29e82
Aman Pratap <singh07ap@gmail.com>
2023-07-08 02:22:27 +0530

    First commit-Initial stage

---
+++ script.js
@@ -0,0 +1,44 @@
+
+// all div id's required for
+
+// search-box
+// searchBtn
+// city-name
+// temperature
+// precipitation
+// day-weather
+
+ const searchBox=document.getElementById("search-box");
+const searchBtn=document.getElementById("searchBtn");
+const cityName=document.getElementById("city-name");
+const temperature=document.getElementById("temperature");
+const wind=document.getElementById("wind");
+const precipitation=document.getElementById("precipitation");
+const dayWeather=document.getElementById("day-weather");
+
+const API_KEY='0e67e7bc781649838f1200425230707'
+
+WeatherData = (city) => {
+	const url = 'http://api.weatherapi.com/v1/current.json';
+	//HINT: Use template literals to create a url with input and an API key
+	fetch(`${url}?key=${API_KEY}&q=${city}&aqi=yes`)
+	.then(response=>response.json())
+	.then(data=>{
+		console.log(data);
+		//printing the city Name
+		cityName.innerText=city;
+		//printing the weather conditions
+		dayWeather.innerHTML=` <h5 id="day">${data.current.condition.text}</h5><img src='${data.current.condition.icon}'> `
+		temperature.innerText="Temp: "+data.current.temp_c+"℃";
+		wind.innerText="Wind: "+data.current.wind_kph+"kph";		
+		precipitation.innerText="Precipitation: "+data.current.precip_mm+"mm";
+
+	})
+	.catch(err=>console.log(err));
+
+  }
+//calling through button
+  searchBtn.onclick=()=>WeatherData(searchBox.value);
+  
+
+
